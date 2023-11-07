be63c4ba87d4df201e93fc353299d2e34426dbdd
NahuelNour23 <lucasnahuelnour2016@gmail.com>
2023-11-07 14:43:37 -0300

    update 2

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const path = require('path');
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'views/home.html'))
+  res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.get('/register.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+
+app.get('/login.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/login.html'));
+});
+app.get('/home.html', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
 app.listen(port, () => {
   console.log('Servidor Corriendo');
-})
+});
